05cc97f6e89c7a8dc8afed3feba37d2483a4b701
Elena Semaan <51868423+sciencezombie@users.noreply.github.com>
2020-12-17 13:31:43 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>The Blog</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+};
+</script>
+
+<style scooped>
+ h1 {
+margin: 30px;
+text-align: center; 
+}
+ 
+</style>
